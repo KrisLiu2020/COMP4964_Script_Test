9006fd8dea8733b5f56d13917734083e9dd7da26
PinghuaLiu <crystalliu9438@gmail.com>
2023-09-24 23:28:05 -0700

    Modified push script

--- mygit-push.sh
+++ mygit-push.sh
@@ -8,6 +8,8 @@ if [[ $push_result == *"Everything up-to-date"* ]]; then
     echo "Push was successful."
 elif [[ $push_result == *"fatal: No configured push destination"* ]]; then
     echo "Error: No push destination configured. You may need to set a remote repository for this branch."
+    echo "Either specify the URL from the command-line or configure a remote repository using: "
+    echo "git remote add origin <remote_repo_url>"
     exit 1
 else
     echo "Push failed."
